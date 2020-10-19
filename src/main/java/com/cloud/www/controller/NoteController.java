0a0d4f8e17ccbc0aeb9bc4412b69d5d277d88905
qichangsong <656079279@qq.com>
2020-10-19 09:50:12 +0800

    增加除了前端以外的插入功能

--- src/main/java/com/cloud/www/controller/NoteController.java
+++ src/main/java/com/cloud/www/controller/NoteController.java
@@ -1,13 +1,20 @@
 package com.cloud.www.controller;
 
+import com.cloud.www.entity.NoteContent;
+import com.cloud.www.service.impl.NoteContentServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import sun.misc.Contended;
 
-@Controller
+@RestController
 public class NoteController {
-    @RequestMapping("/insertNote")
-    void insertNote(){
-
+    @Autowired
+    NoteContentServiceImp noteContentServiceImp;
+    @PostMapping("/insert")
+    String insertNote(@RequestBody NoteContent noteContent){
+         noteContentServiceImp.insertNote(noteContent);
+         System.out.println(noteContent.toString());
+         return noteContent.toString();
     }
 }
